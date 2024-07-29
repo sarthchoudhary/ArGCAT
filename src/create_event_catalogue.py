a9ddba9c961cf4e752f97fa54c7d4a5ae47e78ad
Sarthak Choudhary <sarthak@camk.edu.pl>
2024-07-29 13:57:34 +0200

    skips empty arrays when creating DataFrame.

--- src/create_event_catalogue.py
+++ src/create_event_catalogue.py
@@ -6,7 +6,7 @@ from termcolor import colored
 from tqdm import tqdm
 from pyreco.manager.manager import Manager
 from time import perf_counter
-import h5py
+# import h5py
 from os import path
 
 
@@ -14,8 +14,9 @@ def create_event_catalogue(dir_name:str, filename:str, output_dir:str) -> None:
 
     file_path = path.join(dir_name, filename)
     file_basename = filename.split(sep='.')[0]
-    outfile = 'temPyR00061'
-    confile = 'argset.ini'
+    outfile = 'temPyR00124'
+    # confile = 'argset.ini'
+    confile = 'argset_custom.ini'
     cmdline_args = f'--config {confile} -o {outfile} -i {file_path}'
     pyreco_manager = Manager( midas=True, cmdline_args=cmdline_args)
 
@@ -31,7 +32,7 @@ def create_event_catalogue(dir_name:str, filename:str, output_dir:str) -> None:
 
     start_count = perf_counter()
     for event_index, event in enumerate(pyreco_manager.midas):
-        if event.event_counter > 0:
+        if (event.event_counter > 0) and (len(event.adc_data) != 0):
             # event_index_ls.append(event_index)
             event_counter_ls.append(event.event_counter)
             wf_ls.append(event.adc_data - np.vstack(event.adc_baseline))
@@ -65,24 +66,24 @@ def create_event_catalogue(dir_name:str, filename:str, output_dir:str) -> None:
     print('time taken:', perf_counter() - start_count)
 
     start_count = perf_counter()
-    if file_format == 'h5':
-        print(f'creating h5 container')
-        with h5py.File('data/event_catalogue_{file_basename}.h5', 'w') as event_catalogue: #TODO: dynamic 
-            # event_catalogue.create_dataset('wf', data = wf_ls, compression="gzip")
-            event_catalogue.create_dataset('event_counter', data = event_counter_ls, compression="gzip")
-            event_catalogue.create_dataset('wf_ch0', data = wf_stack_ch0, compression="gzip")
-            event_catalogue.create_dataset('wf_ch1', data = wf_stack_ch1, compression="gzip")
-            event_catalogue.create_dataset('wf_ch2', data = wf_stack_ch2, compression="gzip")
+    # if file_format == 'h5':
+    #     print(f'creating h5 container')
+    #     with h5py.File('data/event_catalogue_{file_basename}.h5', 'w') as event_catalogue: #TODO: dynamic 
+    #         # event_catalogue.create_dataset('wf', data = wf_ls, compression="gzip")
+    #         event_catalogue.create_dataset('event_counter', data = event_counter_ls, compression="gzip")
+    #         event_catalogue.create_dataset('wf_ch0', data = wf_stack_ch0, compression="gzip")
+    #         event_catalogue.create_dataset('wf_ch1', data = wf_stack_ch1, compression="gzip")
+    #         event_catalogue.create_dataset('wf_ch2', data = wf_stack_ch2, compression="gzip")
     print('time taken:', perf_counter() - start_count)
 
 def main() -> None:
-    dir_name = '/work/sarthak/ArgSet/2024_Mar_27/midas' #TODO dynmic
+    dir_name = '/work/sarthak/argset/data/' #TODO dynmic
     # filename = 'run00061.mid.lz4'
     # filename = 'run00052.mid.lz4'
     # create_event_catalogue(dir_name, filename, output_dir)
-    output_dir = '/home/sarthak/my_projects/argset/data'
-    files_ls = ['run00052.mid.lz4', 'run00053.mid.lz4', 'run00054.mid.lz4', \
-        'run00061.mid.lz4', 'run00062.mid.lz4', 'run00063.mid.lz4']
+    output_dir = '/work/sarthak/argset/data/event_catalogues'
+    # files_ls = ['run00108.mid.lz4']
+    files_ls = ['run00124.mid.lz4']
     for filename in files_ls:
         create_event_catalogue(dir_name, filename, output_dir)
 
