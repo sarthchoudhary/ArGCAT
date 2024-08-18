079b260a87781f5bc756916cf2ba82052e5162e6
Sarthak Choudhary <sarthak@camk.edu.pl>
2024-08-18 05:30:00 +0200

    cut-2 and cut-3 interchanged.

--- src/selection_cuts.py
+++ src/selection_cuts.py
@@ -257,11 +257,11 @@ wf_sum_post_cut_ls = []
 for event_x in range(wfs.shape[0]):
     if pretrigger_sum[0][event_x] <= 4000 and pretrigger_sum[0][event_x] >= -6000:
         wf_sum_post_cut_dict[1].append(wf_sum_dict[0][event_x]) # sum is always taken from channel 0; should we change it?
-        if pulse_difference(event_x, use_flt_wf=True) <= 40: # 2nd cut: simultaneity of pulses
+        # if (np.abs(com_dict[0][event_x] - com_dict[1][event_x]) <= com_threshold) and (np.abs(com_dict[2][event_x] - com_dict[1][event_x]) <= com_threshold): # 3rd cut: concurrence of COM
+        if (com_dict[ch_id][event_x] <= com_above_xsigma)[ch_id] and (com_dict[ch_id][event_x] >= com_below_xsigma[ch_id]): # 3rd cut: distance from mean COM
         # if True:
             wf_sum_post_cut_dict[2].append(wf_sum_dict[0][event_x])
-            # if (np.abs(com_dict[0][event_x] - com_dict[1][event_x]) <= com_threshold) and (np.abs(com_dict[2][event_x] - com_dict[1][event_x]) <= com_threshold): # 3rd cut: concurrence of COM
-            if (com_dict[ch_id][event_x] <= com_above_xsigma)[ch_id] and (com_dict[ch_id][event_x] >= com_below_xsigma[ch_id]): # 3rd cut: distance from mean COM
+            if pulse_difference(event_x, use_flt_wf=True) <= 40: # 2nd cut: simultaneity of pulses
                 wf_sum_post_cut_dict[3].append(wf_sum_dict[0][event_x])
                 event_list_post_cut.append(event_x) # these events should be pickled or passed for further processing
                 # np.save('list_pass_events.npy', np.array(event_list_post_cut)) # diag TODO: same for failed events
@@ -281,9 +281,9 @@ fig_2.suptitle('successive cuts')
 ratio_1_0 = np.divide(bin_content_1, bin_content_0, out=np.zeros_like(bin_content_1), where=bin_content_0!=0)
 ax_2[4].plot(bin_edges[:-1], 1.-ratio_1_0, alpha=0.5, color='#f86f6c', label = '1st cut')
 ratio_2_1 = np.divide(bin_content_2, bin_content_1, out=np.zeros_like(bin_content_2), where=bin_content_1!=0)
-ax_2[4].plot(bin_edges[:-1], 1.-ratio_2_1, alpha=0.5, color='#69ebd2', label = '2nd cut')
+ax_2[4].plot(bin_edges[:-1], 1.-ratio_2_1, alpha=0.5, color='#447acd', label = '2nd cut')
 ratio_3_2 = np.divide(bin_content_3, bin_content_2, out=np.zeros_like(bin_content_3), where=bin_content_2!=0)
-ax_2[4].plot(bin_edges[:-1], 1.-ratio_3_2, alpha=0.5, color='#447acd', label = '3rd cut')
+ax_2[4].plot(bin_edges[:-1], 1.-ratio_3_2, alpha=0.5, color='#69ebd2', label = '3rd cut')
 ratio_3_0 = np.divide(bin_content_3, bin_content_0, out=np.zeros_like(bin_content_3), where=bin_content_0!=0)
 ax_2[4].plot(bin_edges[:-1], 1.-ratio_3_0, alpha=0.5, color='#e59acf', label = 'overall')
 ax_2[4].axhline(y=0.2, linestyle='--')
