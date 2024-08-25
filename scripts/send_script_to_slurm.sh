946943d8399bb90d507f8cadec886cd84a923202
Sarthak Choudhary <sarthak@camk.edu.pl>
2024-08-25 22:36:04 +0200

    includes new scripts.

--- scripts/send_script_to_slurm.sh
+++ scripts/send_script_to_slurm.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-##SBATCH --job-name=test_large_midas_files    # Job name
+##SBATCH --job-name=No_Name    # Job name
 #SBATCH --mail-type=ALL               # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=sarthak@camk.edu.pl   # Where to send mail
 #SBATCH --ntasks=1                    # Run on a single CPU
-#SBATCH --time=2-12:00:00               # Time limit hrs:min:sec (you may not want this)
+#SBATCH --time=6:00:00               # Time limit hrs:min:sec (you may not want this)
 #SBATCH --output=%N-%j.out   # Standard output and error log
-#SBATCH --mem=16G                    # Allocated 250 megabytes of memory for the job.
-###SBATCH -A bejger-grp
+#SBATCH --mem=35G                    # Allocated 250 megabytes of memory for the job.
+#SBATCH -A bejger-grp
 #SBATCH --partition=bigmem
 ###SBATCH --partition=dgx
 ##SBATCH --gres=gpu:turing:1                # specify gpu
 
-
-python /home/sarthak/my_projects/argset/src/create_event_catalogue.py
+# python /home/sarthak/my_projects/argset/src/create_event_catalogue.py
 # python /home/sarthak/my_projects/argset/src/calculate_wf_param.py
+# python /home/sarthak/my_projects/argset/src/cut_eff_comparison.py
+python /home/sarthak/my_projects/argset/src/selection_cuts.py
+# python /home/sarthak/my_projects/argset/src/truncate_wfs.py
+# python /home/sarthak/my_projects/argset/src/hist2d_eventID_sum.py
